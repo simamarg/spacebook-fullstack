e1ce8579e5efa10893a6c4703a93563c1b312b19
Sima Margulis Davidi <simamarg@gmail.com>
2018-01-15 23:21:36 +0200

    add post to db and remove from db

--- public/main.js
+++ public/main.js
@@ -1,114 +1,146 @@
-var SpacebookApp = function() {
+var SpacebookApp = function () {
 
-  var posts = [];
+    var posts = [];
 
-  var $posts = $(".posts");
+    var $posts = $(".posts");
 
-  _renderPosts();
-
-  function _renderPosts() {
-    $posts.empty();
-    var source = $('#post-template').html();
-    var template = Handlebars.compile(source);
-    for (var i = 0; i < posts.length; i++) {
-      var newHTML = template(posts[i]);
-      console.log(newHTML);
-      $posts.append(newHTML);
-      _renderComments(i)
-    }
-  }
-
-  function addPost(newPost) {
-    posts.push({ text: newPost, comments: [] });
     _renderPosts();
-  }
 
+    var fetch = function() {
+        $.ajax({
+            method: "GET",
+            url: 'posts',
+            success: function (data) {
+                console.log(data);
+                posts = data;
+                _renderPosts();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
+            }
+        });
+    };
 
-  function _renderComments(postIndex) {
-    var post = $(".post")[postIndex];
-    $commentsList = $(post).find('.comments-list')
-    $commentsList.empty();
-    var source = $('#comment-template').html();
-    var template = Handlebars.compile(source);
-    for (var i = 0; i < posts[postIndex].comments.length; i++) {
-      var newHTML = template(posts[postIndex].comments[i]);
-      $commentsList.append(newHTML);
+    function addPost(newPost) {
+        // posts.push({ text: newPost, comments: [] });
+        // _renderPosts();
+        $.post('posts', { text: newPost });
+        fetch();
     }
-  }
 
-  var removePost = function(index) {
-    posts.splice(index, 1);
-    _renderPosts();
-  };
+    var removePost = function (id) {
+        // posts.splice(index, 1);
+        // _renderPosts();
+        var url = 'posts/' + id;
+        console.log(url);
+        $.ajax({
+            method: "DELETE",
+            url: url,
+            success: function (data) {
+                console.log(data);
+                fetch();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
+            }
+        });
+    };
 
-  var addComment = function(newComment, postIndex) {
-    posts[postIndex].comments.push(newComment);
-    _renderComments(postIndex);
-  };
+    var addComment = function (newComment, postIndex) {
+        posts[postIndex].comments.push(newComment);
+        _renderComments(postIndex);
+    };
 
 
-  var deleteComment = function(postIndex, commentIndex) {
-    posts[postIndex].comments.splice(commentIndex, 1);
-    _renderComments(postIndex);
-  };
+    var deleteComment = function (postIndex, commentIndex) {
+        posts[postIndex].comments.splice(commentIndex, 1);
+        _renderComments(postIndex);
+    };
 
-  return {
-    addPost: addPost,
-    removePost: removePost,
-    addComment: addComment,
-    deleteComment: deleteComment,
-  };
+    function _renderPosts() {
+        $posts.empty();
+        var source = $('#post-template').html();
+        var template = Handlebars.compile(source);
+        for (var i = 0; i < posts.length; i++) {
+            var newHTML = template(posts[i]);
+            // console.log(newHTML);
+            $posts.append(newHTML);
+            _renderComments(i);
+        }
+    }
+
+    function _renderComments(postIndex) {
+        var post = $('.post')[postIndex];
+        $commentsList = $(post).find('.comments-list')
+        $commentsList.empty();
+        var source = $('#comment-template').html();
+        var template = Handlebars.compile(source);
+        for (var i = 0; i < posts[postIndex].comments.length; i++) {
+            var newHTML = template(posts[postIndex].comments[i]);
+            $commentsList.append(newHTML);
+        }
+    }
+
+    return {
+        addPost: addPost,
+        removePost: removePost,
+        addComment: addComment,
+        deleteComment: deleteComment,
+        fetch: fetch,
+        posts: posts
+    };
 };
 
 var app = SpacebookApp();
 
+app.fetch();
 
-$('#addpost').on('click', function() {
-  var $input = $("#postText");
-  if ($input.val() === "") {
-    alert("Please enter text!");
-  } else {
-    app.addPost($input.val());
-    $input.val("");
-  }
+$('#addpost').on('click', function () {
+    var $input = $("#postText");
+    if ($input.val() === "") {
+        alert("Please enter text!");
+    } else {
+        app.addPost($input.val());
+        $input.val("");
+    }
 });
 
 var $posts = $(".posts");
 
-$posts.on('click', '.remove-post', function() {
-  var index = $(this).closest('.post').index();;
-  app.removePost(index);
+$posts.on('click', '.remove-post', function () {
+    var id = $(this).closest('.post').data().id;
+    app.removePost(id);
 });
 
-$posts.on('click', '.toggle-comments', function() {
-  var $clickedPost = $(this).closest('.post');
-  $clickedPost.find('.comments-container').toggleClass('show');
+$posts.on('click', '.toggle-comments', function () {
+    var $clickedPost = $(this).closest('.post');
+    $clickedPost.find('.add-comment-form').toggleClass('show');
 });
 
-$posts.on('click', '.add-comment', function() {
+$posts.on('click', '.add-comment', function () {
 
-  var $comment = $(this).siblings('.comment');
-  var $user = $(this).siblings('.name');
+    var $comment = $(this).siblings('.comment');
+    var $user = $(this).siblings('.name');
 
-  if ($comment.val() === "" || $user.val() === "") {
-    alert("Please enter your name and a comment!");
-    return;
-  }
+    if ($comment.val() === "" || $user.val() === "") {
+        alert("Please enter your name and a comment!");
+        return;
+    }
 
-  var postIndex = $(this).closest('.post').index();
-  var newComment = { text: $comment.val(), user: $user.val() };
+    var postIndex = $(this).closest('.post').index();
+    var newComment = { text: $comment.val(), user: $user.val() };
 
-  app.addComment(newComment, postIndex);
+    app.addComment(newComment, postIndex);
 
-  $comment.val("");
-  $user.val("");
+    $comment.val("");
+    $user.val("");
 
 });
 
-$posts.on('click', '.remove-comment', function() {
-  var $commentsList = $(this).closest('.post').find('.comments-list');
-  var postIndex = $(this).closest('.post').index();
-  var commentIndex = $(this).closest('.comment').index();
+$posts.on('click', '.remove-comment', function () {
+    var $commentsList = $(this).closest('.post').find('.comments-list');
+    var postIndex = $(this).closest('.post').index();
+    var commentIndex = $(this).closest('.comment').index();
 
-  app.deleteComment(postIndex, commentIndex);
+    app.deleteComment(postIndex, commentIndex);
 });
